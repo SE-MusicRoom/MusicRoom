de219c2161c12351dd3ab7e530d66400645ec21c
Kittinan <kyttpwned@hotmail.com>
2017-11-07 16:10:15 +0700

    [REMOVE] remove setter of username & password in User

--- netbeanProject/src/MusicRoom/entity/User.java
+++ netbeanProject/src/MusicRoom/entity/User.java
@@ -47,16 +47,8 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public List<Calendar> getBookedTimes() {
